f2f1eada6b813ad2bf0c79b1a61664d14d7dd8d0
Pfftz <Kalixpfft+github@protonmail.com>
2023-09-22 10:27:10 +0700

    baprot4

---
+++ Bapro/bpif.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int nilai;
+    char x[10];
+    printf("\nMasukkan nilai: ");
+    scanf("%d", &nilai);
+    if(nilai>=60)
+    {
+        strcpy(x, "Lulus\n");
+    }
+    else
+    {
+        strcpy(x, "Tidak Lulus\n");
+    }
+    printf("\n\n %s", x);
+
+    printf("\n Program Selesai.\n");
+    return 0;
+}
