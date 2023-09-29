0a890909105279475c24cfd0deddf75bccaefb22
Pfftz <Kalixpfft+github@protonmail.com>
2023-09-29 09:49:50 +0700

    latihan3

--- Bapro/week5.c
+++ Bapro/week5.c
@@ -1,27 +1,56 @@
+/* #include <stdio.h>
+
+// int main()
+// {
+//     int A, B, C, max = 0;
+//     printf("Input 3 buah bilangan\n");
+//     printf("Bilangan 1 : ");
+//     scanf("%i", &A);
+//     printf("Bilangan 2 : ");
+//     scanf("%i", &B);
+//     printf("Bilangan 3 : ");
+//     scanf("%i", &C);
+
+//     printf("Bilangan terbesar : ");
+
+//     if (A > max)
+//     {
+//         max = A;
+//     }
+//     if (B > max)
+//     {
+//         max = B;
+//     }
+//     if (C > max)
+//     {
+//         max = C;
+//     }
+//     printf("%i\n", max);
+//     return 0;
+// } */
+
 #include <stdio.h>
 
-int main(){
-    int A,B,C;
+int main()
+{
+    int A, max = 0;
     printf("Input 3 buah bilangan\n");
-    printf("Bilangan 1 : ");scanf("%i",&A);
-    printf("Bilangan 2 : ");scanf("%i",&B);
-    printf("Bilangan 3 : ");scanf("%i",&C);
-
-    printf("Bilangan terbesar : ");
-
-    if(A > B){
-        if(A > C){
-            printf("%i",A);
-        } else {
-            printf("%i",C);
-        }
-    } else{
-        if (B > C){
-            printf("%i", B);
-        } else {
-            printf("%i", C);
-        }
+    printf("Bilangan 1 : ");
+    scanf("%i", &A);
+    max = A;
+    printf("Bilangan 2 : ");
+    scanf("%i", &A);
+    if (A > max)
+    {
+        max = A;
     }
+    printf("Bilangan 3 : ");
+    scanf("%i", &A);
+    if (A > max)
+    {
+        max = A;
+    }
+    printf("Bilangan terbesar : ");
+    printf("%i\n", max);
     return 0;
-
 }
