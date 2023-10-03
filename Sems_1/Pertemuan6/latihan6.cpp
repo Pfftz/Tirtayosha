b77ccb4093c25bbd488acab3b0ee32dcaac5ae7a
Pfftz <Kalixpfft+github@protonmail.com>
2023-10-03 19:37:30 +0700

    pertemuan6 well done sir

--- Sems_1/Pertemuan6/latihan6.cpp
+++ Sems_1/Pertemuan6/latihan6.cpp
@@ -3,20 +3,16 @@ using namespace std;
 
 int main()
 {
-    double alas, tinggi, luasSegitiga, panjang, lebar, luasPersegiPanjang;
-    int pilihan, n;
+    int pilihan;
     bool menu = true;
-
+    
     cout << "\n====================================" << endl;
     cout << " Main Menu" << endl;
-    cout << " 1. Menghitung Bilangan Prima" << endl;
-    cout << " 2. Menghitung faktorial" << endl;
-    cout << " 3. Menghitung luas persegi panjang" << endl;
-    cout << " 4. Menghitung luas segitiga" << endl;
-    cout << " 5. Nested Loop" << endl;
-    cout << " 6. Nested Loop Terbalik" << endl;
-    cout << " 7. Prima Checker" << endl;
-    cout << " 8. Keluar" << endl;
+    cout << " 1. Input Nilai Matrix" << endl;
+    cout << " 2. Matrix kali" << endl;
+    cout << " 3. Rata-rata" << endl;
+    cout << " 4. Hilang" << endl;
+    cout << " 5. Keluar" << endl;
     cout << "====================================" << endl;
 
     while (menu)
@@ -29,40 +25,167 @@ int main()
         switch (pilihan)
         {
         case 1:
-            
+        {
+            int baris, kolom;
+
+            cout << " Masukkan jumlah baris: ";
+            cin >> baris;
+            cout << " Masukkan jumlah kolom: ";
+            cin >> kolom;
+
+            int data[baris][kolom];
+
+            for (int i = 0; i < baris; i++)
+            {
+                for (int j = 0; j < kolom; j++)
+                {
+                    cout << " Data [" << i << "][" << j << "] : ";
+                    cin >> data[i][j];
+                }
+            }
+
+            cout << endl;
+            cout << " Data yang telah dimasukkan: " << endl;
+
+            for (int q = 0; q < baris; q++)
+            {
+                for (int e = 0; e < kolom; e++)
+                {
+                    cout << " Data Ke-" << (q * kolom + e) + 1 << ": " << data[q][e] << endl;
+                }
+            }
+
             cout << endl;
             break;
+        }
         case 2:
         {
-           
+            int baris, kolom, kali;
+
+            cout << " Masukkan jumlah baris: ";
+            cin >> baris;
+            cout << " Masukkan jumlah kolom: ";
+            cin >> kolom;
+            cout << " Masukkan jumlah kali: ";
+            cin >> kali;
+
+            int data[baris][kolom];
+
+            for (int i = 0; i < baris; i++)
+            {
+                for (int j = 0; j < kolom; j++)
+                {
+                    cout << " Data [" << i << "][" << j << "] : ";
+                    cin >> data[i][j];
+                }
+            }
+
+            cout << endl;
+            cout << " Data yang telah dimasukkan: " << endl;
+
+            for (int q = 0; q < baris; q++)
+            {
+                for (int e = 0; e < kolom; e++)
+                {
+                    cout << " Data Ke-" << (q * kolom + e) + 1 << ": " << data[q][e] * kali << endl;
+                }
+            }
+
             cout << endl;
             break;
         }
         case 3:
-            
+        {
+            int baris, kolom, counter = 0;
+            float jumlah = 0, rata;
+
+            cout << " Masukkan jumlah baris: ";
+            cin >> baris;
+            cout << " Masukkan jumlah kolom: ";
+            cin >> kolom;
+
+            int data[baris][kolom];
+
+            for (int i = 0; i < baris; i++)
+            {
+                for (int j = 0; j < kolom; j++)
+                {
+                    cout << " Data [" << i << "][" << j << "] : ";
+                    cin >> data[i][j];
+                }
+            }
+
+            cout << endl;
+            cout << " Data yang telah dimasukkan: " << endl;
+
+            for (int q = 0; q < baris; q++)
+            {
+                for (int e = 0; e < kolom; e++)
+                {
+                    cout << " Data Ke-" << (q * kolom + e) + 1 << ": " << data[q][e] << endl;
+                    jumlah += data[q][e];
+                    counter++;
+                }
+            }
+            rata = jumlah / counter;
+            cout << " Jumlah data: " << jumlah << endl;
+            cout << " Rata-rata data: " << rata << endl;
+
             cout << endl;
             break;
+        }
         case 4:
-            
+        {
+            int data[2];
+            system("cls");
+            for (int i = 0; i < 2; i++)
+            {
+                cout << "\n====================================" << endl;
+                cout << " Main Menu" << endl;
+                cout << " 1. Input Nilai Matrix" << endl;
+                cout << " 2. Matrix kali" << endl;
+                cout << " 3. Rata-rata" << endl;
+                cout << " 4. Hilang" << endl;
+                cout << " 5. Keluar" << endl;
+                cout << "====================================" << endl;
+                cout << endl;
+                cout << " Masukkan pilihan: 4";
+                cout << endl;
+                cout << " Masukkan Nilai : ";
+                cin >> data[i];
+                system("cls");
+            }
+            cout << "\n====================================" << endl;
+            cout << " Main Menu" << endl;
+            cout << " 1. Input Nilai Matrix" << endl;
+            cout << " 2. Matrix kali" << endl;
+            cout << " 3. Rata-rata" << endl;
+            cout << " 4. Hilang" << endl;
+            cout << " 5. Keluar" << endl;
+            cout << "====================================" << endl;
+            cout << endl;
+            cout << " Masukkan pilihan: 4";
+            cout << endl;
+
+            int hasil = 1;
+            string formula = " Hasil: ";
+            for (int i = 0; i < 2; i++)
+            {
+                hasil *= data[i];
+                formula += to_string(data[i]);
+                if (i < 2 - 1)
+                {
+                    formula += " * ";
+                }
+            }
+            formula += " = " + to_string(hasil);
+
+            cout << formula << endl;
+
             cout << endl;
             break;
+        }
         case 5:
-            
-            cout << endl;
-            break;
-        case 6:
-        {
-            
-            cout << endl;
-            break;
-        }
-        case 7:
-        {
-            
-            cout << endl;
-            break;
-        }
-        case 8:
             cout << " <<program selesai>>" << endl;
             exit(0);
         default:
