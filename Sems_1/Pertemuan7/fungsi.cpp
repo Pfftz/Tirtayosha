06742384932702b0ea0dd1343f192b3d4460c611
Pfftz <Kalixpfft+github@protonmail.com>
2023-10-10 14:23:30 +0700

    menu 5

--- Sems_1/Pertemuan7/fungsi.cpp
+++ Sems_1/Pertemuan7/fungsi.cpp
@@ -42,26 +42,196 @@ void menu1()
     rumus1(a, b);
 }
 
+void menu2()
+{
+    int databelumurut[6] = {5, 3, 4, 2, 1, 5}, n = 6;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (databelumurut[j] > databelumurut[j + 1])
+            {
+                int temp = databelumurut[j];
+                databelumurut[j] = databelumurut[j + 1];
+                databelumurut[j + 1] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << databelumurut[i] << " ";
+    }
+    cout << endl;
+}
+
+void menu3()
+{
+    int length;
+    cout << " Masukkan panjang array: ";
+    cin >> length;
+
+    int databelumurut[length];
+    for (int i = 0; i < length; i++)
+    {
+        cout << " Data ke-" << i + 1 << " : ";
+        cin >> databelumurut[i];
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length - i - 1; j++)
+        {
+            if (databelumurut[j] > databelumurut[j + 1])
+            {
+                int temp = databelumurut[j];
+                databelumurut[j] = databelumurut[j + 1];
+                databelumurut[j + 1] = temp;
+            }
+        }
+    }
+    cout << "\n Angka setelah diurutkan: ";
+    for (int i = 0; i < length; i++)
+    {
+        cout << " " << databelumurut[i] << " ";
+    }
+    cout << endl;
+}
+
+void menu4()
+{
+    int length;
+    cout << " Masukkan panjang array: ";
+    cin >> length;
+
+    int databelumurut[length];
+    for (int i = 0; i < length; i++)
+    {
+        cout << " Data ke-" << i + 1 << " : ";
+        cin >> databelumurut[i];
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length - i - 1; j++)
+        {
+            if (databelumurut[j] < databelumurut[j + 1])
+            {
+                int temp = databelumurut[j];
+                databelumurut[j] = databelumurut[j + 1];
+                databelumurut[j + 1] = temp;
+            }
+        }
+    }
+    cout << "\n Angka setelah diurutkan: ";
+    for (int i = 0; i < length; i++)
+    {
+        cout << " " << databelumurut[i] << " ";
+    }
+    cout << endl;
+}
+
+void menu5()
+{
+    int length;
+    cout << " Masukkan panjang array: ";
+    cin >> length;
+
+    char databelumurut[length];
+    for (int i = 0; i < length; i++)
+    {
+        cout << " Data ke-" << i + 1 << " : ";
+        cin >> databelumurut[i];
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        for (int j = 0; j < length - i - 1; j++)
+        {
+            if (databelumurut[j] > databelumurut[j + 1])
+            {
+                char temp = databelumurut[j];
+                databelumurut[j] = databelumurut[j + 1];
+                databelumurut[j + 1] = temp;
+            }
+        }
+    }
+    cout << "\n Angka setelah diurutkan: ";
+    for (int i = 0; i < length; i++)
+    {
+        cout << " " << databelumurut[i] << " ";
+    }
+    cout << endl;
+}
+
+void menu6(){
+
+}
+
 int main()
 {
-    int i = 1, menu;
+    int pilihan;
+    bool menu = true;
 
-    do
+    while (menu)
     {
-        cout << "1. Menghitung Phytagoras" << endl;
-        cout << "2. Menghitung Diskriminan" << endl;
-        cout << "Silahkan pilih menu: ";
-        cin >> menu;
+        cout << "\n Menu: " << endl;
+        cout << " 1. Menghitung Phytagoras" << endl;
+        cout << " 2. Bubble sort" << endl;
+        cout << " 3. Bubble sorting dinamis" << endl;
+        cout << " 4. Bubble sorting dinamis terbalik" << endl;
+        cout << " 5. Bubble sorting dinamis Huruf" << endl;
+        cout << " 6. Exit" << endl;
+        cout << " Silahkan pilih menu: ";
+        cin >> pilihan;
+        cout << endl;
 
-        if (menu == 1)
+        switch (pilihan)
         {
+        case 1:
             menu1();
+            cout << endl;
+            system("pause");
+            system("cls");
+            break;
+        case 2:
+            menu2();
+            cout << endl;
+            system("pause");
+            system("cls");
+            break;
+        case 3:
+            menu3();
+            cout << endl;
+            system("pause");
+            system("cls");
+            break;
+        case 4:
+            menu4();
+            cout << endl;
+            system("pause");
+            system("cls");
+            break;
+        case 5:
+            menu5();
+            cout << endl;
+            system("pause");
+            system("cls");
+            break;
+        case 6:
+            menu6();
+            cout << endl;
+            system("pause");
+            system("cls");
+            break;
+        default:
+            cout << " Menu tidak tersedia" << endl;
+            system("pause");
+            system("cls");
+            break;
         }
-        else if (menu == 2)
-        {
-            // menu2();
-        }
-    } while (i > 0);
+    }
 
     return 0;
 }
