9f847f93988272ce25150d27ad33c7514312c814
Pfftz <Kalixpfft+github@protonmail.com>
2023-10-16 18:29:08 +0700

    lmao

--- gabutz/coba.cpp
+++ gabutz/coba.cpp
@@ -3,17 +3,26 @@
 using namespace std;
 
 int main(){
-    int x1, x2, y1, y2;
-    float jarak;
-    cout << "Masukkan nilai x1 : ";
-    cin >> x1;
-    cout << "Masukkan nilai x2 : ";
-    cin >> x2;
-    cout << "Masukkan nilai y1 : ";
-    cin >> y1;
-    cout << "Masukkan nilai y2 : ";
-    cin >> y2;
-    jarak = sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
-    cout << "Jarak antara titik (" << x1 << "," << y1 << ") dan (" << x2 << "," << y2 << ") adalah " << jarak << endl;
-    return 0;
+    int n;
+    float total = 1;
+    cout << "Masukan bilangan prima\n";
+    cin >> n;
+    for (int a = 1; a < n; a++)
+    {
+        int bil = 0;
+        for (int b = 1; b <= a; b++)
+        {
+            if (a % b == 0)
+            {
+                bil = bil + 1;
+            }
+        }
+        if (bil == 2)
+        {
+            cout << "bilamgan prima nya adalah : " << a << endl;
+            total *= a;
+        }
+    }
+
+    cout << "Total semua bilangan prima: " << total << endl;
 }
