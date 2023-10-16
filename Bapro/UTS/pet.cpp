92af2ccc2995a8b11d4b3e2c7b801f05ca2c09e2
Pfftz <Kalixpfft+github@protonmail.com>
2023-10-16 21:44:58 +0700

    gg

--- Bapro/UTS/pet.cpp
+++ Bapro/UTS/pet.cpp
@@ -1,33 +1,93 @@
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
+
 int xyz;
-string nama[5];
-string kode[5] = {"A-1234", "B-2345", "A-3345", "D-5433", "F-3840"};
-string jenis[5] = {"Kucing", "Anjing", "Ular", "Anjing", "Kucing"};
+
+void input(int xyz, string nama[], int kode[], string jenis[])
+{
+    for (int o = 0; o < xyz; o++)
+    {
+        cout << endl;
+        cout << " =============================================" << endl;
+        cout << "\tData " << o + 1 << endl;
+        cout << "\tNama Hewan " << o + 1 << ": ";
+        cin >> nama[o];
+        cout << "\tKode\t    : ";
+        kode[o] = rand() % (100 - 0 + 1) + 0;
+        cout << kode[o] << endl;
+        cout << "\tJenis Hewan : " << endl;
+        cout << "\t  1. Kucing" << endl;
+        cout << "\t  2. Anjing" << endl;
+        cout << "\t  3. Kelinci" << endl;
+        cout << "\t  4. Hamster" << endl;
+        cout << "\t  5. Burung" << endl;
+        cout << "\t  6. Ikan" << endl;
+        cout << "\t  7. Reptil" << endl;
+        cout << "\t  8. Dragon" << endl;
+        cout << "\t  Pilih Jenis Hewan : ";
+        int pilih;
+        cin >> pilih;
+        switch (pilih)
+        {
+        case 1:
+            jenis[o] = "Kucing";
+            break;
+        case 2:
+            jenis[o] = "Anjing";
+            break;
+        case 3:
+            jenis[o] = "Kelinci";
+            break;
+        case 4:
+            jenis[o] = "Hamster";
+            break;
+        case 5:
+            jenis[o] = "Burung";
+            break;
+        case 6:
+            jenis[o] = "Ikan";
+            break;
+        case 7:
+            jenis[o] = "Reptil";
+            break;
+        case 8:
+            jenis[o] = "Dragon";
+            break;
+        default:
+            jenis[o] = "Tidak Ada";
+            break;
+        }
+        cout << " =============================================" << endl;
+    }
+}
+
+void output(int xyz, string nama[], int kode[], string jenis[])
+{
+    cout << endl
+         << "\n\tDaftar Data Hewan Peliharaan" << endl;
+    cout << " =============================================" << endl;
+    cout << " No\tNama               Kode \tJenis" << endl;
+    cout << " =============================================" << endl;
+    for (int g = 0; g < xyz; g++)
+    {
+        cout << " " << g + 1 << "\t" << setw(15) << nama[g] << "\t" << kode[g] << "\t" << jenis[g]
+             << endl;
+    }
+    cout << "=============================================" << endl;
+}
+
 int main()
 {
     cout << "\t<<<Program Penitipan Hewan>>>" << endl;
     cout << "\t <<<Pet Care Synchro>>>" << endl;
-    cout << "\n\nInput Data Hewan yang Akan Dititipkan: ";
+    cout << "\n\n Input Data Hewan yang Akan Dititipkan: ";
     cin >> xyz;
-    for (int o = 0; o < xyz; o++)
-    {
-        cout << " Data " << o + 1 << endl;
-        cout << "\tNama Hewan " << o + 1 << ": ";
-        cin >> nama[o];
-        cout << "\tKode\t : " << kode[o] << endl;
-        cout << "\tJenis Hewan : " << jenis[o] << endl;
-    }
-    cout << endl
-         << "\n\tDaftar Data Hewan Peliharaan" << endl;
-    cout << "=============================================" << endl;
-    cout << "No\tNama \tKode \tJenis" << endl;
-    cout << "=============================================" << endl;
-    for (int g = 0; g < xyz; g++)
-    {
-        cout << g + 1 << "\t" << nama[g] << "\t" << kode[g] << "\t" << jenis[g]
-             << endl;
-    }
-    cout << "=============================================" << endl;
+    string nama[xyz];
+    int kode[xyz];
+    string jenis[xyz];
+    input(xyz, nama, kode, jenis);
+    output(xyz, nama, kode, jenis);
     return 0;
 }
