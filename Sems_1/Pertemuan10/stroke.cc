76a59eddc6680ae26d35cb84cd4d1941f013834a
Pfftz <Kalixpfft+github@protonmail.com>
2023-11-07 17:40:21 +0700

    Add struct and parking fee calculation functions

--- Sems_1/Pertemuan10/stroke.cc
+++ Sems_1/Pertemuan10/stroke.cc
@@ -1,20 +1,59 @@
+// Struct
 #include <iostream>
 
 using namespace std;
 
-void record1(){
+struct mahasiswa
+{
+    string nim;
+    string nama;
+    string jurusan;
+    int nilai;
+};
 
+void stroke1(){
+    /*
+    data.nim = "23214322";
+    data.nama = "Bayu";
+    data.jurusan = "Teknik Informatika";
+    data.nilai = 86;
+
+    cout << "Nim: " << data.nim << endl;
+    cout << "nama: " << data.nama << endl;
+    cout << "jurusan: " << data.jurusan << endl;
+    cout << "nilai: " << data.nilai << endl;
+    */
 }
 
-void record2(){
+int main()
+{
 
-}
+    int jumlahData;
+    cout << "Masukkan jumlah data: ";
+    cin >> jumlahData;
+    mahasiswa data[jumlahData];
 
-void record3(){
+    for (int i = 0; i < jumlahData; i++)
+    {
+        cout << "Data Mahasiswa ke-" << i + 1 << endl;
+        cout << "Masukkan Nim: ";
+        cin >> data[i].nim;
+        cout << "Masukkan Nama: ";
+        cin >> data[i].nama;
+        cout << "Masukkan Jurusan: ";
+        cin >> data[i].jurusan;
+        cout << "Masukkan Nilai: ";
+        cin >> data[i].nilai;
+    }
+    cout << endl;
 
-}
-
-int main() {
-    
+    for (int i = 0; i < jumlahData; i++)
+    {
+        cout << "Tampilan Data Mahasiswa ke-" << i + 1 << endl;
+        cout << "Nim: " << data[i].nim << endl;
+        cout << "Nama: " << data[i].nama << endl;
+        cout << "Jurusan: " << data[i].jurusan << endl;
+        cout << "Nilai: " << data[i].nilai << endl;
+    }
     return 0;
 }
---
+++ Sems_1/Pertemuan10/struct_041.cc
@@ -0,0 +1,170 @@
+#include <iostream>
+
+using namespace std;
+
+struct mahasiswa
+{
+    int nim;
+    string nama;
+};
+
+struct parkir
+{
+    int jam, menit, detik;
+    int selisih, biaya;
+};
+
+void menu1(int *pn)
+{
+    cout << " Masukkan nilai n: ";
+    cin >> *pn;
+
+    mahasiswa *data = new mahasiswa[*pn];
+
+    int temp;
+    string temp2;
+    for (int i = 0; i < *pn; i++)
+    {
+        cout << " Masukkan data mahasiswa ke-" << i + 1 << ": " << endl;
+        cout << " NIM[0]    : ";
+        cin >> data[i].nim;
+        cout << " Nama[1]   : ";
+        cin >> data[i].nama;
+        cout << endl;
+    }
+
+    for (int i = 0; i < *pn; i++)
+    {
+        for (int j = i + 1; j < *pn; j++)
+        {
+            if (data[i].nim < data[j].nim)
+            {
+                temp = data[i].nim;        // nim[i]
+                data[i].nim = data[j].nim; // nim[i] = nim[j]
+                data[j].nim = temp;        // nim[j] = temp
+
+                temp2 = data[i].nama;        // nama[i]
+                data[i].nama = data[j].nama; // nama[i] = nama[j]
+                data[j].nama = temp2;        // nama[j] = temp2
+            }
+        }
+    }
+
+    cout << " Hasil akhir setelah diurutkan berdasarkan NIM adalah: " << endl;
+    for (int i = 0; i < *pn; i++)
+    {
+        cout << " (NIM: " << data[i].nim;
+        cout << " Nama: " << data[i].nama << ") " << endl;
+    }
+
+    cout << endl;
+    system("pause");
+    system("cls");
+}
+
+void menu2()
+{
+    parkir arrived, departure;
+    cout << " Input arrived time" << endl;
+    cout << " Hour: ";
+    cin >> arrived.jam;
+    cout << " Minute: ";
+    cin >> arrived.menit;
+    cout << " Second: ";
+    cin >> arrived.detik;
+    cout << endl;
+    cout << " Input departure time" << endl;
+    cout << " Hour: ";
+    cin >> departure.jam;
+    cout << " Minute: ";
+    cin >> departure.menit;
+    cout << " Second: ";
+    cin >> departure.detik;
+    cout << endl;
+
+    if (departure.detik < arrived.detik)
+    {
+        departure.detik += 60;
+        departure.menit--;
+    }
+    else if (departure.menit < arrived.menit)
+    {
+        departure.menit += 60;
+        departure.jam--;
+    }
+    else if (departure.jam < arrived.jam)
+    {
+        departure.jam += 24;
+    }
+    else if (departure.jam == arrived.jam && departure.menit == arrived.menit && departure.detik == arrived.detik)
+    {
+        cout << " Arrived time = " << arrived.jam << ":" << arrived.menit << ":" << arrived.detik << endl;
+        cout << " Departure time = " << departure.jam << ":" << departure.menit << ":" << departure.detik << endl;
+        cout << " Parking time = 0:0:0" << endl;
+        cout << " Parking fee = 0" << endl;
+        exit(0);
+    }
+
+    cout << " Arrived time = " << arrived.jam << ":" << arrived.menit << ":" << arrived.detik << endl;
+    cout << " Departure time = " << departure.jam << ":" << departure.menit << ":" << departure.detik << endl;
+    departure.jam -= arrived.jam;
+    departure.menit -= arrived.menit;
+    departure.detik -= arrived.detik;
+    cout << " Parking time = " << departure.jam << ":" << departure.menit << ":" << departure.detik << endl;
+    departure.selisih = departure.jam * 3600 + departure.menit * 60 + departure.detik;
+    if (departure.selisih <= 3600)
+    {
+        departure.biaya = 3000;
+    }
+    else if (departure.selisih > 3600)
+    {
+        departure.biaya = 3000;
+        departure.biaya += 3000; 
+    }
+
+    cout << " Parking fee = " << departure.biaya << endl;
+    cout << endl;
+    system("pause");
+    system("cls");
+}
+
+void menu3()
+{
+    cout << " Terima kasih telah menggunakan program ini" << endl;
+    exit(0);
+}
+
+int main()
+{
+    int pilih, n;
+    bool menu = true;
+    while (menu)
+    {
+        cout << "\n====================================" << endl;
+        cout << " Main Menu" << endl;
+        cout << " 1. Mengurutkan nim mahasiswa descending" << endl;
+        cout << " 2. Mencari Jumlah biaya parkir dari selisih datang dan pergi" << endl;
+        cout << " 3. Keluar" << endl;
+        cout << "====================================" << endl;
+        cout << endl;
+        cout << " Masukkan pilihan: ";
+        cin >> pilih;
+        cout << endl;
+        switch (pilih)
+        {
+        case 1:
+            menu1(&n);
+            break;
+        case 2:
+            menu2();
+            break;
+        case 3:
+            menu3();
+            break;
+        default:
+            cout << " Pilihan tidak tersedia" << endl;
+            break;
+        }
+    }
+    return 0;
+}
