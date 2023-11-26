be15f4a1f451a835b4d13c7bb52548211704177f
Pfftz <Kalixpfft+github@protonmail.com>
2023-11-26 22:01:59 +0700

    Add lambda function to main.cpp

---
+++ SI/lambda.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 5;
+    int b = 6;
+    int c = 7;
+    int d = 8;
+
+    auto f = [a, &b, &c, d]()
+    {
+        cout << a << endl;
+        cout << b << endl;
+        cout << c << endl;
+        cout << d << endl;
+    };
+
+    a = 10;
+    b = 11;
+    c = 12;
+    d = 13;
+
+    f();
+
+    return 0;
+}
