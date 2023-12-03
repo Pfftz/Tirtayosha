1b98cda04f2087286a85f61157febb076484aa99
Pfftz <Kalixpfft+github@protonmail.com>
2023-12-03 12:30:19 +0700

    Add tuple manipulation code

---
+++ SI/tuple.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <tuple>
+#include <algorithm>
+#include <array>
+
+using namespace std;
+
+int main()
+{
+    tuple<int, int, int> t = {1, 3, 8};
+    tuple<int, int, int> t2 = {2, 1, 2};
+
+    auto f = [&t]()
+    {
+        cout << get<0>(t) << " " << get<1>(t) << " " << get<2>(t) << endl;
+    };
+
+    auto urut = [&t]()
+    {
+        array<int, 3> a = {get<0>(t), get<1>(t), get<2>(t)};
+        sort(a.begin(), a.end());
+        t = make_tuple(a[0], a[1], a[2]);
+    };
+
+    auto terbalik = [&t]()
+    {
+        array<int, 3> a = {get<0>(t), get<1>(t), get<2>(t)};
+        reverse(a.begin(), a.end());
+        t = make_tuple(a[0], a[1], a[2]);
+    };
+
+    auto tambah = [&t, &t2]()
+    { tuple<int, int, int, int, int, int> t3 = tuple_cat(t, t2); 
+      cout << get<0>(t3) << " " << get<1>(t3) << " " << get<2>(t3) << " " << get<3>(t3) << " " << get<4>(t3) << " " << get<5>(t3) << endl; };
+
+    f();
+    urut();
+    cout << endl;
+    f();
+    cout << endl;
+    terbalik();
+    f();
+    cout << endl;
+    tambah();
+    cout << endl;
+
+    return 0;
+}
