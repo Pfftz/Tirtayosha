704c14bc8e65259736f09e0012cee1ccc3abcc7c
Pfftz <Kalixpfft+github@protonmail.com>
2023-12-05 14:19:28 +0700

    Refactor QUIZ.cc file

--- Sems_1/Pertemuan14/QUIZ.cc
+++ Sems_1/Pertemuan14/QUIZ.cc
@@ -4,22 +4,20 @@ using namespace std;
 
 struct buku
 {
-    string nama;
-    int harga, stock, kode;
-    bool status;
+    string nama, kode;
+    int stock;
     buku *next;
 };
 
 void tambahDataAwal(buku **head)
 {
     buku *nodeBaru = new buku();
-    cout << "Masukkan kode buku: ";
+    cout << " Masukkan kode buku: ";
     cin >> nodeBaru->kode;
-    cout << "Masukkan nama buku: ";
-    cin >> nodeBaru->nama;
-    cout << "Masukkan harga buku: ";
-    cin >> nodeBaru->harga;
-    cout << "Masukkan stock buku: ";
+    cout << " Masukkan nama buku: ";
+    cin.ignore();
+    getline(cin, nodeBaru->nama);
+    cout << " Masukkan stock buku: ";
     cin >> nodeBaru->stock;
     nodeBaru->next = NULL;
 
@@ -36,13 +34,12 @@ void tambahDataAwal(buku **head)
 void tambahDataEnd(buku **head)
 {
     buku *nodeBaru = new buku();
-    cout << "Masukkan kode buku: ";
+    cout << " Masukkan kode buku: ";
     cin >> nodeBaru->kode;
-    cout << "Masukkan nama buku: ";
-    cin >> nodeBaru->nama;
-    cout << "Masukkan harga buku: ";
-    cin >> nodeBaru->harga;
-    cout << "Masukkan stock buku: ";
+    cout << " Masukkan nama buku: ";
+    cin.ignore();
+    getline(cin, nodeBaru->nama);
+    cout << " Masukkan stock buku: ";
     cin >> nodeBaru->stock;
     nodeBaru->next = NULL;
 
@@ -66,60 +63,36 @@ void displayData(buku *head)
 
     if (temp == NULL)
     {
-        cout << "Linkedlist masih kosong" << endl;
+        cout << " Linkedlist masih kosong" << endl;
         return;
     }
 
     while (temp != NULL)
     {
-        cout << "Kode buku: " << temp->kode << endl;
-        cout << "Nama buku: " << temp->nama << endl;
-        cout << "Harga buku: " << temp->harga << endl;
-        cout << "Stock buku: " << temp->stock << endl;
+        cout << " Kode buku: " << temp->kode << endl;
+        cout << " Nama buku: " << temp->nama << endl;
+        cout << " Stock buku: " << temp->stock << endl;
+        if (temp->stock > 0)
+            cout << " Status buku: Tersedia" << endl;
+        else
+            cout << " Status buku: tidak tersedia" << endl;
         cout << endl;
         temp = temp->next;
     }
-}
-
-void ubahData(buku **head)
-{
-    int kode;
-    cout << "Masukkan kode buku yang ingin diubah: ";
-    cin >> kode;
-
-    buku *temp = (*head);
-    while (temp != NULL)
-    {
-        if (temp->kode == kode)
-        {
-            cout << "Masukkan kode buku: ";
-            cin >> temp->kode;
-            cout << "Masukkan nama buku: ";
-            cin >> temp->nama;
-            cout << "Masukkan harga buku: ";
-            cin >> temp->harga;
-            cout << "Masukkan stock buku: ";
-            cin >> temp->stock;
-            cout << endl;
-            return;
-        }
-        temp = temp->next;
-    }
-    cout << "Kode buku tidak ditemukan" << endl;
 }
 
 void hapusData(buku **head)
 {
     if (*head == NULL)
     {
-        cout << "linked list nya masih kosong" << endl;
+        cout << " linked list nya masih kosong" << endl;
         return;
     }
 
     if ((*head)->next == NULL)
     {
         *head = NULL;
-        cout << "Node terlah terhapus" << endl;
+        cout << " Node terlah terhapus" << endl;
         return;
     }
 
@@ -129,22 +102,42 @@ void hapusData(buku **head)
         temp = temp->next;
     }
     temp->next = NULL;
-    cout << "Node berhasil dihapus" << endl;
+    cout << " Node berhasil dihapus" << endl;
+}
+
+bool isBookAvailable(buku **head, string kode)
+{
+    buku *temp = *head;
+    while (temp != NULL)
+    {
+        if (temp->kode == kode)
+        {
+            return temp->stock > 0;
+        }
+        temp = temp->next;
+    }
+    return false;
 }
 
 void transaksi(buku **head)
 {
     if (*head == NULL)
     {
-        cout << "linked list nya masih kosong" << endl;
+        cout << " linked list nya masih kosong" << endl;
         return;
     }
     buku *temp = (*head);
-    int kode, total;
-    string tanggal_awal, tanggal_akhir;
+    int total;
+    string tanggal_awal, tanggal_akhir, kode;
 
-    cout << "Masukkan kode buku: ";
+    cout << " Masukkan kode buku: ";
     cin >> kode;
+
+    if (!isBookAvailable(head, kode))
+    {
+        cout << " Buku tidak tersedia" << endl;
+        return;
+    }
 
     while (temp != NULL)
     {
@@ -153,13 +146,9 @@ void transaksi(buku **head)
             if (temp->stock > 0)
             {
                 temp->stock--;
-                if (temp->stock == 0)
-                {
-                    temp->status = "Tidak tersedia";
-                }
-                cout << "Masukkan tanggal peminjaman (format: dd-mm-yyyy): ";
+                cout << " Masukkan tanggal peminjaman (format: dd-mm-yyyy): ";
                 cin >> tanggal_awal;
-                cout << "Masukkan tanggal pengembalian (format: dd-mm-yyyy): ";
+                cout << " Masukkan tanggal pengembalian (format: dd-mm-yyyy): ";
                 cin >> tanggal_akhir;
 
                 // Assuming the dates are in the format "dd-mm-yyyy"
@@ -168,18 +157,18 @@ void transaksi(buku **head)
                 int total_days = day_akhir - day_awal;
 
                 total = total_days * 5000;
-                cout << "Total biaya peminjaman: " << total << endl;
+                cout << " Total biaya peminjaman: " << total << endl;
                 return;
             }
             else
             {
-                cout << "Buku tidak tersedia" << endl;
+                cout << " Buku tidak tersedia" << endl;
                 return;
             }
         }
         temp = temp->next;
     }
-    cout << "Kode buku tidak ditemukan" << endl;
+    cout << " Kode buku tidak ditemukan" << endl;
 }
 
 int main()
@@ -189,15 +178,16 @@ int main()
     bool isMenu = true;
     while (isMenu)
     {
-        cout << "===MENU===" << endl;
-        cout << "1. Tambah Data" << endl;
-        cout << "2. Ubah Data" << endl;
-        cout << "3. Hapus Data" << endl;
-        cout << "4. Tampil Data" << endl;
-        cout << "5. Transaksi" << endl;
-        cout << "9. Exit" << endl;
+        cout << "\n Peminjaman Buku" << endl;
+        cout << "+---------------------+" << endl;
+        cout << " 1. Tambah Data" << endl;
+        cout << " 2. Hapus Data" << endl;
+        cout << " 3. Tampil Data" << endl;
+        cout << " 4. Transaksi" << endl;
+        cout << " 0. Exit" << endl;
+        cout << "+---------------------+" << endl;
 
-        cout << "Silahkan Pilih Menu: ";
+        cout << " Silahkan Pilih Menu: ";
         cin >> menu;
 
         switch (menu)
@@ -205,24 +195,40 @@ int main()
         case 1:
             tambahDataAwal(&HEAD);
             // tambahDataEnd(&HEAD);
+            cout << endl;
+            system("pause");
+            system("cls");
             break;
         case 2:
-            ubahData(&HEAD);
+            hapusData(&HEAD);
+            cout << endl;
+            system("pause");
+            system("cls");
             break;
         case 3:
-            hapusData(&HEAD);
+            displayData(HEAD);
+            cout << endl;
+            system("pause");
+            system("cls");
             break;
         case 4:
-            displayData(HEAD);
-            break;
-        case 5:
             transaksi(&HEAD);
+            cout << endl;
+            system("pause");
+            system("cls");
             break;
-        case 9:
+        case 0:
             isMenu = false;
+            cout << "   ______    __          __        ______    " << endl;
+            cout << "  /\" _  \"\\  |\" \\        /\"\"\\      /    \" \\   " << endl;
+            cout << " (: ( \\___) ||  |      /    \\    // ____  \\  " << endl;
+            cout << "  \\/ \\      |:  |     /' /\\  \\  /  /    ) :) " << endl;
+            cout << "  //  \\ _   |.  |    //  __'  \\(: (____/ //  " << endl;
+            cout << " (:   _) \\  /\\  |\\  /   /  \\\\  \\        /   " << endl;
+            cout << "  \\_______)(__\\_|_)(___/    \\___)\"_____/    " << endl;
             break;
         default:
-            cout << "Pilihan tdk ada !" << endl;
+            cout << " Pilihan tdk ada !" << endl;
         }
     }
     return 0;
